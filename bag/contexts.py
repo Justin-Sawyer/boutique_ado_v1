0a16460d44a3ee5b140b12e02e7323104c169332
Justin-Sawyer <justin.sawyer@icloud.com>
2021-05-28 14:07:45 +0000

    Added checkout success logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -34,7 +34,7 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PRECENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
